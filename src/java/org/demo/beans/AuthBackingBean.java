866a38b1f988b9d011dc62d68a3bf93966bc9653
Kundan <kundanparajuli@gmail.com>
2014-11-28 14:17:12 -0600

    security installed

---
+++ src/java/org/demo/beans/AuthBackingBean.java
@@ -0,0 +1,40 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package org.demo.beans;
+
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import javax.inject.Named;
+import javax.enterprise.context.Dependent;
+import javax.faces.context.FacesContext;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletRequest;
+
+/**
+ *
+ * @author you
+ */
+@Named(value = "authBackingBean")
+@Dependent
+public class AuthBackingBean {
+
+    private static Logger log = Logger.getLogger(AuthBackingBean.class.getName());
+
+    public String logout() {
+        String result = "/index?faces-redirect=true";
+
+        FacesContext context = FacesContext.getCurrentInstance();
+        HttpServletRequest request = (HttpServletRequest) context.getExternalContext().getRequest();
+
+        try {
+            request.logout();
+        } catch (ServletException e) {
+            log.log(Level.SEVERE, "Failed to logout user!", e);
+            result = "/loginError?faces-redirect=true";
+        }
+
+        return result;
+    }
+}
