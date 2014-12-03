53c5ab01572b4d392bc33aeb58920e5eedcb986e
Kundan <kundanparajuli@gmail.com>
2014-12-03 12:21:40 -0600

    Maven Edited

---
+++ src/java/com/promed/users/IPerson.java
@@ -0,0 +1,14 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.promed.users;
+
+/**
+ *
+ * @author Keshav
+ */
+public interface IPerson {
+    
+}
